9cc6941a9c4f18ced8b62e142abc270769958ec8
joshsrai <joshsrai@gmail.com>
2020-08-23 16:37:26 -0400

    2020-08-23 4:37pm

--- lib/events/events.rb
+++ lib/events/events.rb
@@ -1,11 +1,11 @@
 class Events
-    attr_accessor :selection
+    attr_accessor :input
 
-    def self.chosen_event 
-        selected_event = Scraper.find(self.selection)
-        selected_event_date = Scraper.find_date(self.selection)
+    def self.chosen_event(input) 
+        selected_event = Scraper.find(input)
+        selected_event_date = Scraper.find_date(input)
         selected_event_name = selected_event.css('div.col-xs-7.col-md-7 p').first.text
-        puts "You have selected: #{self.selection}. #{selected_event_name}"
+        puts "You have selected: #{input}. #{selected_event_name}"
         puts "#{selected_event_date}"
     end
 
@@ -13,15 +13,14 @@ class Events
         Scraper.get_events
         Scraper.print_events
         puts "choose an event."
-        binding.pry
-        self.selection = gets.chomp.to_i
-        if self.selection > 0 && self.selection < 117
-            self.chosen_event
+        input = gets.chomp.to_i
+        if input > 0 && input < 117
+            self.chosen_event(input)
         else 
-            puts "invalid selection\n"
+            puts "Invalid selection.\n"
             puts "Choose another event."
-            self.selection = gets.chomp.to_i
-            self.chosen_event
+            input = gets.chomp.to_i
+            self.chosen_event(input)
         end
     end
 end
--- lib/events/scraper.rb
+++ lib/events/scraper.rb
@@ -10,19 +10,11 @@ class Scraper
     def self.print_events
         @@doc.css('.events-list.all.row').each_with_index do |event, indx|
             puts "#{indx +1}. " + event.css('div.col-xs-7.col-md-7 p').first.text
-            #puts event.css('.date-list').first.text.strip
         end
     end
 
-    # def self.print_events_date
-    #     @@doc.css('.events-list.all.row').each_with_index do |event, indx|
-    #         puts "event.css('div.col-xs-2.col-md-2.date-list p').text
-    #     end
-    # end
-
     def self.find(index)
         @@doc.css('.events-list.all.row')[index.to_i - 1]
-        #@@doc.css('.div.col-xs-2.col-md-2.date-list')[index.to_i - 1]
     end
 
     def self.find_date(index)
