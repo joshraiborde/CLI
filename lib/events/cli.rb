f45d6068e41c90ad8208f5d0f29b7b9489d7d3a0
joshsrai <joshsrai@gmail.com>
2020-08-30 17:06:14 -0400

    2020-08-30 5:06pm

--- lib/events/cli.rb
+++ lib/events/cli.rb
@@ -6,10 +6,8 @@ class CLI
         self.display_events
         puts "Choose an event."
         input = gets.chomp.to_i
-        #binding.pry
         if input > 0 && input < Events.all.length
             self.show_event(input)
-            #Events.chosen_event(input)
         else 
             puts "Invalid selection.\n"
             puts "Choose another event."
@@ -33,67 +31,7 @@ class CLI
 
     def self.show_event(index)
         chosen_event = Events.all.find{|event| index == event.index}
-        #binding.pry
         puts "You have selected: #{index}. #{chosen_event.name}"
         puts chosen_event.date
     end
 end
-
-#     def call
-#         Scraper.get_events
-#         self.start
-
-#     end
-
-# def list_events
-#     Scraper.print_events
-
-# end
-    # require_relative "cli/version"
-    # require  "pry"
-    # require "open-uri"
-    # require "nokogiri"
-
-    # require_relative "cli/scraper"
-
-
-    # def start
-    #     puts "Welcome to the Lexington Community Events page."
-    #     puts "These are the events happening this year"
-    #     puts "Please select a numbered event:"
-    #     puts "---------------------------"
-    #     Events.start
-    #     (should i delete?? the lines below)
-    #     input = gets.chomp.to_i
-    #     if input > 0 && input < 117
-    #         Events.chosen_event(input)
-    #     else 
-    #         puts "Invalid selection.\n"
-    #         puts "Choose another event."
-    #         input = gets.chomp.to_i
-    #         Events.chosen_event(input)
-    #     end
-        
-
-    # end
-
-    # def call
-    #     input = ""
-    #     while input != "exit"
-        #     puts "Welcome to the Lexington Community Events page."
-    #     puts "These are the events happening this year"
-    #     puts "Please select a numbered event:"
-    #     puts "---------------------------"
-    #     Events.start
-    #       input = gets.strip
-    #       case input
-    #       when "list"
-    #         list_articles
-    #       when "exit"
-    #         puts "Goodbye"
-    #         break
-    #       else
-    #         system("open #{@newsletter.articles[input.to_i-1].url}")
-    #       end
-    #     end
-    #   end
--- lib/events/scraper.rb
+++ lib/events/scraper.rb
@@ -14,57 +14,8 @@ class Scraper
         doc = @@doc.css('.events-list.all.row')
         index = 1
         @@doc.css('.events-list.all.row').each do |name|
-            #binding.pry
             event = Events.new(name.css('div.col-xs-7.col-md-7 p').first.text.strip, name.css('div.col-xs-2.col-md-2.date-list p').first.text.strip, index)
-            
-            # event.date = name.css('div')[0].css('p')[0].text.strip
             index += 1
         end
     end
 end
-
-    # def self.find(index)
-    #     @@doc.css('.events-list.all.row')[index.to_i - 1]
-    # end
-
-    # def self.find_date(index)
-    #     @@doc.css('.date-list')[index.to_i - 1].text.strip
-    # end
-
-# class Scraper
-
-#     def self.get_events
-#         @@doc = Nokogiri::HTML(open("http://www.lexingtonsc.org/community-events"))        
-#     end
-
-#     def self.print_events
-#         @@doc.css('.events-list.all.row').each_with_index do |event, indx|
-#             puts "#{indx +1}. " + event.css('div.col-xs-7.col-md-7 p').first.text
-#         end
-#     end
-
-#     def self.find(index)
-#         @@doc.css('.events-list.all.row')[index.to_i - 1]
-#     end
-
-#     def self.find_date(index)
-#         @@doc.css('.date-list')[index.to_i - 1].text.strip
-#     end
-# end
-
-
-
-#     def self.get_events
-#         html= open("http://www.lexingtonsc.org/community-events")
-#         doc = Nokogiri::HTML(html)
-#         events_table = doc.css(".chamber-events")
-#         event = events_table.css(".col-xs-7 p")
-#         event.each do |e|
-#         #binding.pry
-#         Event.new()
-#         end
-#         # @@doc = Nokogiri::HTML(open("http://www.lexingtonsc.org/community-events"))
-#     end
-# end
-# Scraper.get_events
-# Scraper.create_events
